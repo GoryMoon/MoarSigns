71a6f76ec39e520485017dea4174d073e19d7411
GoryMoon <gurreja@gmail.com>
2014-05-04 12:28:45 +0200

    Update version number

--- src/main/java/gory_moon/moarsigns/lib/ModInfo.java
+++ src/main/java/gory_moon/moarsigns/lib/ModInfo.java
@@ -3,7 +3,7 @@ package gory_moon.moarsigns.lib;
 public class ModInfo {
     public static final String ID = "MoarSigns";
     public static final String NAME = "MoarSigns";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     public static final String CHANNEL_S = "moarsignsS";
     public static final String CHANNEL_C = "moarsignsC";
     public static final String COMMONPROXY = "gory_moon.moarsigns.proxy.CommonProxy";
