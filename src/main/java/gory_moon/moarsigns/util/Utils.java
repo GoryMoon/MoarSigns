dd5e9191cb397ee63ec583cf88597e493620df38
GoryMoon <gurreja@gmail.com>
2014-08-03 15:08:45 +0200

    Forgot to commit this in cc977e39e98ed7590073195e976a97107294192e

--- src/main/java/gory_moon/moarsigns/util/Utils.java
+++ src/main/java/gory_moon/moarsigns/util/Utils.java
@@ -6,8 +6,9 @@ public class Utils {
         return size > 15 ? 1 : (size > 5 ? 2 : (size > 1 ? 3 : 4));
     }
 
+    private static int[] maxLenghts = {90, 82, 76, 70, 66, 62, 58, 54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 36, 34, 32, 32};
     public static int getMaxLength(int size) {
-        return size > 17 ? 5 : (size > 13 ? 6 : (size > 10 ? 7 : (size > 7 ? 8 : (size > 4 ? 9 : (size > 3 ? 11 : (size > 1 ? 12 : (size > 0 ? 13 : 15)))))));
+        return maxLenghts[size];
     }
 
 }
