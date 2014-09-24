8ec84724e7ca9c19b327149ca392a1102c586eb9
GoryMoon <gurreja@gmail.com>
2014-09-24 22:32:24 +0200

    Made the deleting of special signs remove the full text combo

--- src/main/java/gory_moon/moarsigns/client/interfaces/GuiSignTextField.java
+++ src/main/java/gory_moon/moarsigns/client/interfaces/GuiSignTextField.java
@@ -40,9 +40,9 @@ public class GuiSignTextField extends GuiTextField {
     public void writeText(String text) {
         String s1 = "";
         String s2 = ChatAllowedCharacters.filerAllowedCharacters(text);
-        int i = this.getCursorPosition() < this.getSelectionEnd() ? this.getCursorPosition() : this.getSelectionEnd();
-        int j = this.getCursorPosition() < this.getSelectionEnd() ? this.getSelectionEnd() : this.getCursorPosition();
-        int k = this.maxRowLength  - field_146211_a.getStringWidth(GuiMoarSign.getSignTextWithColor(new String[]{this.text})[0]) - (field_146211_a.getStringWidth(this.text.substring(0, i)) - field_146211_a.getStringWidth(this.text.substring(0, j)));// - field_146211_a.getStringWidth(this.text.substring(0, i - this.getSelectionEnd()));
+        int i = cursorPosition < this.selectionEnd ? cursorPosition : this.selectionEnd;
+        int j = cursorPosition < this.selectionEnd ? this.selectionEnd : cursorPosition;
+        int k = this.maxRowLength  - field_146211_a.getStringWidth(GuiMoarSign.getSignTextWithColor(new String[]{this.text})[0]) - (field_146211_a.getStringWidth(this.text.substring(0, i)) - field_146211_a.getStringWidth(this.text.substring(0, j)));// - field_146211_a.getStringWidth(this.text.substring(0, i - this.selectionEnd));
 
         if (this.text.length() > 0)
         {
@@ -77,16 +77,85 @@ public class GuiSignTextField extends GuiTextField {
         this.moveCursorBy(i - this.getSelectionEnd() + l);
     }
 
+    public void deleteFromCursor(int p_146175_1_)
+    {
+        if (this.text.length() != 0)
+        {
+            if (this.selectionEnd != this.cursorPosition)
+            {
+                this.writeText("");
+            }
+            else
+            {
+                boolean flag = p_146175_1_ < 0;
+
+                char[][] position = {{'{'}, {'∫'}, {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c',
+                'd', 'e', 'f', 'k', 'l', 'm', 'n', 'o', 'r'}, {'}'}};
+
+                int offset = getCharIndex(position, this.text.charAt(this.cursorPosition + (flag ? p_146175_1_ : 0) ));
+
+                if (offset > -1 &&
+                        (flag && 0 < cursorPosition - offset - 1 && this.text.length() > this.cursorPosition + (3 - offset) &&
+                            isSpecial(this.text.substring(this.cursorPosition - offset - 1, cursorPosition + (3 - offset)))) ||
+                        (!flag && 0 <  cursorPosition - offset) && this.text.length() > this.cursorPosition + (3 - offset + 1) &&
+                            isSpecial(this.text.substring(this.cursorPosition - offset, cursorPosition + (3 - offset + 1)))
+                    ) {
+
+                    this.selectionEnd = flag ? (cursorPosition + (3 - offset)): (cursorPosition + (3 - offset + 1));
+                    this.cursorPosition = flag ? (cursorPosition - offset - 1): (cursorPosition - offset);
+
+                    this.writeText("");
+
+                } else {
+
+                    int j = flag ? this.cursorPosition + p_146175_1_ : this.cursorPosition;
+                    int k = flag ? this.cursorPosition : this.cursorPosition + p_146175_1_;
+                    String s = "";
+
+                    if (j >= 0) {
+                        s = this.text.substring(0, j);
+                    }
+
+                    if (k < this.text.length()) {
+                        s = s + this.text.substring(k);
+                    }
+
+                    this.text = s;
+
+                    if (flag) {
+                        this.moveCursorBy(p_146175_1_);
+                    }
+                }
+            }
+        }
+    }
+
+    public void setMaxRowLength(int maxRowLength) {
+        this.maxRowLength = maxRowLength;
+    }
+
     private boolean isSpecial(String s) {
-        return s.matches("(\\{[0-9a-z]\\})");
+        return s.matches("(\\{∫[0-9a-z]\\})");
 
     }
 
     private int getSpecialsWidth(String s) {
-        Matcher m = Pattern.compile("(\\{[0-9a-z]\\})").matcher(s);
+        Matcher m = Pattern.compile("(\\{∫[0-9a-z]\\})").matcher(s);
         int i = 0;
 
         while (m.find()) i += field_146211_a.getStringWidth(m.group(1));
         return i;
+    }
+
+    private int getCharIndex(char[][] arr, char c) {
+        int pos = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                pos = arr[i][j] == c ? i: pos;
+            }
+        }
+
+        return pos;
     }
 }
