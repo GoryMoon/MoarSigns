4460103fa8af7a669cfd15774f91f3663beaf46b
GoryMoon <gurreja@gmail.com>
2017-06-08 16:37:31 +0200

    Fixed jenkins downloading libs

--- download-libs.sh
+++ download-libs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -ex
-wget https://www.dropbox.com/s/i4lmhmgf072ekgp/MoarSigns-Libs.zip
+wget -O MoarSigns-Libs.zip https://www.dropbox.com/s/i4lmhmgf072ekgp/MoarSigns-Libs.zip
 unzip -o MoarSigns-Libs.zip
